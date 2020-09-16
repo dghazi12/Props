4d9fd72f3b8a4a1ac2b6f5df8532796d01b4c5d6
David Ghazi <davidaghazi@gmail.com>
2020-09-15 23:51:52 -0400

    create app + login page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+import Login from './Components/Login/LoginPage'
+
+class App extends React.Component {
+  render() {
+    return (
+    <Login />
+  )}
+}
+
+export default App;
